3d7c66d1db87303404d434dc814367347d639263
Crowdbotics <team@crowdbotics.com>
2023-03-30 23:37:01 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "super-shadow-39627",
+  url: "https://super-shadow-39627.botics.co",
+  api: "https://super-shadow-39627.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
